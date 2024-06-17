b1a5efe66a9fc35910094f6aa6cba756eb20ea47
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-06-17 15:43:58 +0530

    Added react hot toast

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Routes>{/* <Route path="/" element={<Home/>}></Route> */}</Routes>
+      <Routes>
+        {/* <Route path="/" element={<Home/>}></Route> */}
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 
 // Library Imports
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 
 // Component Imports
@@ -11,5 +12,6 @@ import App from "./App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
 );
